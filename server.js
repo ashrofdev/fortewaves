539983a2b9e618af5a9134bc2ca5f6b1b512807a
Ashrof Salman <salmanashraf853@gmil.com>
2021-12-14 15:09:39 +0100

    creating routes

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-import express from "express";
+const express = require("express")
+
 
 
 
 
 
 const app = express()
+
+app.get('/yes', (req, res)=>{
+    console.log('accessed')
+    res.json({
+        yes:'yeah'
+    })
+})
+
+
+
+app.listen('3001')
